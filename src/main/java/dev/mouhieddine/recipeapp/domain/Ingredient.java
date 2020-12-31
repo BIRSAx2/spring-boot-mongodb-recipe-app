3d4e11d8e159e26a54fc77c586b5ae2063ed44f6
BIRSAx2 <Mouhieddinesabir@gmail.com>
2020-12-31 16:56:33 +0100

    Fixed recipe/id/show not working issue

--- src/main/java/dev/mouhieddine/recipeapp/domain/Ingredient.java
+++ src/main/java/dev/mouhieddine/recipeapp/domain/Ingredient.java
@@ -5,34 +5,33 @@ import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 
 @Getter
 @Setter
 public class Ingredient {
 
-    private String id = UUID.randomUUID().toString();
-    private String description;
-    private BigDecimal amount;
+  private String id;
+  private String description;
+  private BigDecimal amount;
 
-    @DBRef
-    private UnitOfMeasure uom;
+  @DBRef
+  private UnitOfMeasure uom;
 
-    public Ingredient() {
+  public Ingredient() {
 
-    }
+  }
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
-        this.description = description;
-        this.amount = amount;
-        this.uom = uom;
-    }
+  public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+    this.description = description;
+    this.amount = amount;
+    this.uom = uom;
+  }
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
-        this.description = description;
-        this.amount = amount;
-        this.uom = uom;
-    }
+  public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    this.description = description;
+    this.amount = amount;
+    this.uom = uom;
+  }
 
 }
